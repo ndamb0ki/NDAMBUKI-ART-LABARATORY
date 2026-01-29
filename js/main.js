91d1df254bf0fe41fb92c6d93190e7deef13c0e8
ndamb0ki <121855485+ndamb0ki@users.noreply.github.com>
2026-01-29 18:56:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,3 +26,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+
+navToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
